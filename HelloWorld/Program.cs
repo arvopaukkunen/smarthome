f1b13cfa073a0138acf393a287bba6f62893a7a6
Arvo Paukkunen <arvosoft@hotmail.com>
2023-02-18 23:06:19 +0200

    xx
    xx

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 //this must be in remote git when deploying to Ubuntu
+// remote main branch is empty?
+//dev1 branch created
 namespace HelloWorld
 {
     class Program
@@ -10,7 +12,7 @@ namespace HelloWorld
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.Write($"{Environment.NewLine}Please Press any key to exit...");
             Console.ReadKey(true);
         }
     }
