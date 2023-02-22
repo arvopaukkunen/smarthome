5e766fa71417b60c4aec085e9be047529ab4b2c8
Arvo Paukkunen <arvosoft@hotmail.com>
2023-02-22 15:03:44 +0200

    Agent project skeleton added
    Remember target platform changes - in RaspBerryPi it's 6.0

---
+++ Agent/Model/Sabiana.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Agent.Model
+{
+	public class Sabiana
+	{
+		public Sabiana()
+		{
+		}
+	}
+}
+
---
+++ Agent/Model/Vallox.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Agent.Model
+{
+	public class Vallox
+	{
+		public Vallox()
+		{
+		}
+	}
+}
+
